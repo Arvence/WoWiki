2859fad8a7642c4c81ef56db8bdc082f6e164935
Arvence <acapnes@gmail.com>
2026-07-29 12:55:59 +0300

    chore: remove unused backend helper

--- backend/src/common/errors/find-by-id-or-throw.ts
+++
@@ -1,8 +0,0 @@
-import { NotFoundException } from '@nestjs/common'
-import { EntityWithId } from '../types/entity.type'
-
-export function findByIdOrThrow<T extends EntityWithId>(entities: readonly T[], id: string, entityName: string): T {
-  const entity = entities.find((item) => item.id === id)
-  if (!entity) throw new NotFoundException(`${entityName} with id ${id} not found`)
-  return entity
-}
